1a0de31da39d00052fb07bfe434d98bf9ec17194
yendra164 <yendra960@gmail.com>
2025-09-04 11:05:35 +0700

    upload project belajar_react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+  
+      <h1 style={{ color: "blue", textAlign: "center", }}>
+       Hallo Yendra Selamat Datang di REACT 
+      </h1>
+  );
+}
+
+export default App;
